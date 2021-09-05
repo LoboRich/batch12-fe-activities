0659b2dbcacf94d54c778d70acaf916f08968e99
Richelle Lobo <lobo.r0018@gmail.com>
2021-09-05 09:52:53 +0800

    Added functions for placing mark to cell and switching turns.

--- js-activities/tic-tac-toe/script.js
+++ js-activities/tic-tac-toe/script.js
@@ -1,13 +1,43 @@
+const xClass = 'x';
+const circleClass = 'circle';
+var board = document.getElementById('board');
 // select all data-cell
 var dataCell = document.querySelectorAll('[data-cell]');
 // add click listeners to each cells
-dataCell.forEach(cell => {
-    cell.addEventListener('click', handleClick(), {once: true});
-})
+let xTurn;
 
-function handleClick(){
+start();
+
+function start(){
+    xTurn = true;
+    dataCell.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: true});
+    })
+    setBoardHoverCurrentTurn();
 }
 
-function placeMark(){
+function handleClick(e){
+    const cell = e.target;
+    const currentTurn  = xTurn ? xClass : circleClass;
+    placeMark(cell, currentTurn);
+    switchTurn();
+    setBoardHoverCurrentTurn();
+}
 
+function placeMark(cell, currentTurn){
+    cell.classList.add(currentTurn);
+}
+
+function switchTurn(){
+    xTurn = !xTurn
+}
+
+function setBoardHoverCurrentTurn(){
+    board.classList.remove(xClass);
+    board.classList.remove(circleClass);
+    if(xTurn){
+        board.classList.add(xClass);
+    }else{
+        board.classList.add(circleClass);
+    }
 }
